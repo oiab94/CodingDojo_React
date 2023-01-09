2f15fd6846b083c1af376e7309c8359560e06555
oiab94AsusLaptop <oscar.alonso.994@gmail.com>
2023-01-09 17:59:55 -0300

    [Tarea] Gran Inversion

--- primer-react/src/App.js
+++ primer-react/src/App.js
@@ -1,9 +1,32 @@
 // import logo from "./logo.svg";
 import "./App.css";
-import PersonCard from "./componentesFuncionales/personCard";
+import PersonCard from "./granInversion/personCard";
 
 let App = () => {
-  return <PersonCard name="John" lastName="Smith" age={9} hairColor="Brown" />;
+  return (
+    <div>
+      <PersonCard 
+				name="Jane" 
+				lastName="Doe" 
+				age={45} 
+				hairColor="Black" />
+      <PersonCard 
+				name="John" 
+				lastName="Smith" 
+				age={9} 
+				hairColor="Brown" />
+      <PersonCard
+        name="Millard"
+        lastName="Filmore"
+        age={50}
+        hairColor="Brown"/>
+      <PersonCard 
+				name="Maria" 
+				lastName="Smith" 
+				age={62} 
+				hairColor="Brown" />
+    </div>
+  );
 };
 
 export default App;
